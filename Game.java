0115d92feee63ca0008738ecbef84eeed0994d9d
zlouttit <zackarylouttit@gmail.com>
2022-01-03 14:01:41 -0600

    humanTurn Update
    Added drawing part of human turn

--- Game.java
+++ Game.java
@@ -20,22 +20,74 @@ public class Game {
 
     }
 
-    public void swap(Player player, int playerIndex){
+    private void swap(Player player, int playerIndex){
         Card temp;
         temp = deck[deckIndex];
         deck[deckIndex] = player.getCard(playerIndex);
         player.setCard(temp, playerIndex);
     }
 
-    public int humanTurn(){
+    public void humanTurn(){
         System.out.println(players[0]);
         System.out.println("The card in the discard pile is a/an " + deck[deckIndex].toString() + ".\n");
 
         Scanner in = new Scanner(System.in);
-        System.out.println("Your options are either drawing a card (1), swapping with the discard card (2)," +
-                "or knocking (3). \nWhat would you like to do?");
-        return in.nextInt();
-    }
+        int turnVal = 0;
+        String userMessage;
 
+        while(turnVal < 1 || turnVal > 3) {
+            try {
+                System.out.println("Your options are either drawing a card (1), swapping with the discard card (2)," +
+                        "or knocking (3). \nWhat would you like to do?");
+
+                userMessage = in.next();
+                turnVal = Integer.parseInt(userMessage);
+                if(turnVal < 1 || turnVal > 3)
+                    throw new Exception();
+
+            }catch(Exception e){
+                System.out.println("\nInvalid input, please enter 1, 2, or 3.\n");
+            }
+        }
+
+        if(turnVal < 3) {
+
+            //Drawing a card
+            if (turnVal == 1) {
+                deckIndex++;
+                System.out.println("You have drawn a/an " + deck[deckIndex].toString() + ".\n");
+                turnVal = -1;
+
+                while(turnVal < 0 || turnVal > 3) {
+                    try {
+                        System.out.println("Your options are either discarding (0), or swapping with the " +
+                                players[0].getCard(0).toString() + "(1)," +
+                                players[0].getCard(1).toString() + "(2), or " +
+                                players[0].getCard(2).toString() + "(3)." +
+                                "\nWhat would you like to do?");
+
+                        userMessage = in.next();
+                        turnVal = Integer.parseInt(userMessage);
+                        if(turnVal < 0 || turnVal > 3)
+                            throw new Exception();
+
+                    }catch(Exception e){
+                        System.out.println("\nInvalid input, please enter 0, 1, 2, or 3.\n");
+                    }
+                }
+                if(turnVal > 0)
+                    swap(players[0], turnVal -1 );
+
+            //Swapping with discard card
+            }else{
+
+            }
+
+        // Knocking
+        }else{
+
+        }
+        in.close;
+    }
 
 }
